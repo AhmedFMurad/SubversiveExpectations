b5b4f521be59b82a2bec0019e428ec4153b03adc
Jeffrey <jeffreylim@outlook.com>
2017-10-13 13:54:10 -0400

    "Added users.java"

---
+++ app/src/main/java/com/a3004/tldr/tldr/Users.java
@@ -0,0 +1,15 @@
+package com.a3004.tldr.tldr;
+
+/**
+ * Created by Jeffrey on 10-13-2017.
+ */
+
+public class Users {
+    String userName
+
+    public String getUserName(){
+        return userName;
+    }
+
+
+}
