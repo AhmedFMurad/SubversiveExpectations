d1a9ca577310083acddba3a77288d2bf37bdb0d5
Jeffrey <jeffreylim@outlook.com>
2017-10-13 14:05:08 -0400

    removed a comment

--- app/src/main/java/com/a3004/tldr/tldr/Users.java
+++ app/src/main/java/com/a3004/tldr/tldr/Users.java
@@ -1,11 +1,8 @@
 package com.a3004.tldr.tldr;
 
-/**
- * Created by Jeffrey on 10-13-2017.
- */
 
 public class Users {
-    String userName
+    protected String userName
 
     public String getUserName(){
         return userName;
