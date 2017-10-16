220a3d67383abf70e99c732843c903638a00a478
ahmedfmmurad <ahmadfayez93@gmail.com>
2017-10-16 14:09:32 -0400

    Website class basic structure

--- app/src/test/java/com/a3004/tldr/tldr/Websites.java
+++ app/src/test/java/com/a3004/tldr/tldr/Websites.java
@@ -2,4 +2,9 @@ package com.a3004.tldr.tldr;
 
 
 public class Websites {
+    public String[] rssArray;
+
+    public void fetchArticles(){
+        // This should be on a timer to fetch news automatically or something
+    }
 }
