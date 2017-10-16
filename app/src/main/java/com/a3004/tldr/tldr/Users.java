b895f1bd5e815f1b0a013fe23907803e52241f83
ahmedfmmurad <ahmadfayez93@gmail.com>
2017-10-16 11:19:30 -0400

    Users class basic structure added

--- app/src/main/java/com/a3004/tldr/tldr/Users.java
+++ app/src/main/java/com/a3004/tldr/tldr/Users.java
@@ -1,11 +1,37 @@
 package com.a3004.tldr.tldr;
 
 
+import java.util.Arrays;
+import java.util.Date;
+
+import static java.util.Arrays.asList;
+
 public class Users {
-    protected String userName
+    protected String username;
+    protected String[] preferedCategories;
+    protected Date lastLogin;
+    protected int amountOfCategories;
 
     public String getUserName(){
-        return userName;
+        return username;
+    }
+
+    public void addCategory(String category){
+        if(!(this.preferedCategories.length < this.amountOfCategories)){
+            // expand the array
+        } else {
+            this.preferedCategories[this.amountOfCategories] = category;
+            this.amountOfCategories++;
+        }
+        // Update database?
+    }
+
+    public void removeCategory(String category){
+        int index = Arrays.asList(this.preferedCategories).indexOf(category);
+        for(int i=index; i<this.amountOfCategories;i++){
+            this.preferedCategories[i] = this.preferedCategories[i+1];
+        }
+        this.amountOfCategories--;
     }
 
 
