cd6a2c449079c55cf173e84e6f854e2bd39298b1
ahmedfmmurad <ahmadfayez93@gmail.com>
2017-10-16 16:38:03 -0400

    rss feeds changed to hashmap

--- app/src/main/java/com/a3004/tldr/tldr/Websites.java
+++ app/src/main/java/com/a3004/tldr/tldr/Websites.java
@@ -1,7 +1,9 @@
 package com.a3004.tldr.tldr;
 
+import java.util.Map;
+
 public class Websites {
-    public String[][] rssArray;
+    public Map<String, String> rssArray;
 
     public void fetchArticles(){
         // This should be on a timer to fetch news automatically or something
