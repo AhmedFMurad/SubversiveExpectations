95a79b2a81a083d3543d21137f0cee4bc71e26e2
Jeffrey <jeffreylim@outlook.com>
2017-10-17 13:44:53 -0400

    Renamed Websites to Website

---
+++ app/src/main/java/com/a3004/tldr/tldr/Website.java
@@ -0,0 +1,17 @@
+package com.a3004.tldr.tldr;
+
+import java.util.Map;
+
+public class Website {
+    public Map<String, String> rssArray;
+    private String websiteTitle;
+
+    public void fetchArticles(){
+        // This should be on a timer to fetch news automatically or something
+    }
+
+    public void setWebsiteTitle(){
+        //set the title then move towards UI
+
+    }
+}
