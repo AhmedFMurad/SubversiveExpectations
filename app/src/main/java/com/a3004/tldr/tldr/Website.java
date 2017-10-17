cdcad8190133df230803d01bab70bc19766589a9
Jeffrey <jeffreylim@outlook.com>
2017-10-17 13:48:27 -0400

    Renamed Websites to Website

--- app/src/main/java/com/a3004/tldr/tldr/Website.java
+++ app/src/main/java/com/a3004/tldr/tldr/Website.java
@@ -4,14 +4,10 @@ import java.util.Map;
 
 public class Website {
     public Map<String, String> rssArray;
-    private String websiteTitle;
 
     public void fetchArticles(){
         // This should be on a timer to fetch news automatically or something
     }
 
-    public void setWebsiteTitle(){
-        //set the title then move towards UI
 
-    }
 }
