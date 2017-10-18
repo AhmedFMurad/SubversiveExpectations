f3b0f219a84cda051cdd87268f07270f9691d86f
Jeffrey <jeffreylim@outlook.com>
2017-10-18 17:42:18 -0400

    Changed Website and Category

--- app/src/main/java/com/a3004/tldr/tldr/Website.java
+++ app/src/main/java/com/a3004/tldr/tldr/Website.java
@@ -1,18 +1,25 @@
 package com.a3004.tldr.tldr;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class Website {
-    public Map<String, String> rssArray;
+    private Map<String, String> feeds = new HashMap<>();
     private String siteTitle;
 
     Website(String title){
         siteTitle = title;
     }
 
+    public Map<String, String> getFeeds(){
+        return this.feeds;
+    }
+
     public void fetchArticles(){
         // This should be on a timer to fetch news automatically or something
     }
 
-
+    public void addFeed(String category, String url){
+        feeds.put(category, url);
+    }
 }
