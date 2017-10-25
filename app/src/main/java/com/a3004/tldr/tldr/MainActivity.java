5cb146276fec06deef37ecdeeaade42a0ab69f8d
kkbenny <hxhb1030@gmail.com>
2017-10-25 16:59:56 -0400

    updated

--- app/src/main/java/com/a3004/tldr/tldr/MainActivity.java
+++ app/src/main/java/com/a3004/tldr/tldr/MainActivity.java
@@ -1,23 +1,79 @@
 package com.a3004.tldr.tldr;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.RecyclerView;
-
-import java.util.ArrayList;
+import android.view.Menu;
+import android.view.MenuItem;
 
 
 public class MainActivity extends AppCompatActivity {
-    //private MobileServiceClient mClient;
 
-    // --- recycler view part ---
-    private RecyclerView mRecyclerView;
-    private ArrayList<Article> articles;
+    private SectionsPageAdapter mSectionsPageAdapter;
+    private ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_main);
+
+        mSectionsPageAdapter = new SectionsPageAdapter(getSupportFragmentManager());
+
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        setupViewPager(mViewPager);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(mViewPager);
+
+        tabLayout.getTabAt(0).setIcon(R.drawable.ic_explore_white_18dp);
+        tabLayout.getTabAt(1).setIcon(R.drawable.ic_home_white_18dp);
+        tabLayout.getTabAt(2).setIcon(R.drawable.ic_face_white_18dp);
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavView_Bar);
+        BottomNavigationViewHelper.disableShiftMode(bottomNavigationView);
+        Menu menu = bottomNavigationView.getMenu();
+        MenuItem menuItem = menu.getItem(0);
+        menuItem.setChecked(true);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.ic_explore:
+                        Intent intent1 = new Intent(MainActivity.this, ActivityExplore.class);
+                        startActivity(intent1);
+                        break;
+
+                    case R.id.ic_home:
+                        Intent intent2 = new Intent(MainActivity.this, ActivityHome.class);
+                        startActivity(intent2);
+                        break;
+
+                    case R.id.ic_user:
+                        Intent intent3 = new Intent(MainActivity.this, ActivityUser.class);
+                        startActivity(intent3);
+                        break;
+                }
+
+
+                return false;
+            }
+        });
+
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        SectionsPageAdapter adapter = new SectionsPageAdapter(getSupportFragmentManager());
+        adapter.addFragment(new TabExploreFragment());
+        adapter.addFragment(new TabHomeFragment());
+        adapter.addFragment(new TabUserFragment());
+        viewPager.setAdapter(adapter);
+    }
     }
 
 }
