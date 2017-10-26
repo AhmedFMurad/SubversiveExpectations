e18a62844905147adbb626c73ea4b79d6861ffee
kkbenny <hxhb1030@gmail.com>
2017-10-26 11:31:01 -0400

    added "log in" page for user (not finished yet)

--- app/src/main/java/com/a3004/tldr/tldr/MainActivity.java
+++ app/src/main/java/com/a3004/tldr/tldr/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user_beforelogin);
+        setContentView(R.layout.activity_user_login);
 
         //mSectionsPageAdapter = new SectionsPageAdapter(getSupportFragmentManager());
 
