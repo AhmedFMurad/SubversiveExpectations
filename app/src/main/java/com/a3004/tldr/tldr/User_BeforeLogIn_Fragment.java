bf832f744e2196ef44d1e40f6ca9faf78d1b3de6
kkbenny <hxhb1030@gmail.com>
2017-10-28 16:43:42 -0400

    updated

--- app/src/main/java/com/a3004/tldr/tldr/User_BeforeLogIn_Fragment.java
+++ app/src/main/java/com/a3004/tldr/tldr/User_BeforeLogIn_Fragment.java
@@ -4,6 +4,6 @@ import android.support.v4.app.Fragment;
 
 
 // before user log in fragment
-public class User_BeforeLogInFragment extends Fragment {
+public class User_BeforeLogIn_Fragment extends Fragment {
 
 }
