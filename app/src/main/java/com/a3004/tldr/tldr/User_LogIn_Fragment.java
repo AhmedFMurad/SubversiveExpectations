872071a36a4c7b9c6b58ab3eb8110dc865db3f44
ahmedfmmurad <ahmadfayez93@gmail.com>
2017-10-31 11:41:39 -0400

    update

--- app/src/main/java/com/a3004/tldr/tldr/User_LogIn_Fragment.java
+++ app/src/main/java/com/a3004/tldr/tldr/User_LogIn_Fragment.java
@@ -18,8 +18,9 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 
+
 // user log in fragment
-public class User_LogIn_Fragment extends AppCompatActivity implements View.OnClickListener {
+public class User_LogIn_Fragment extends AppCompatActivity {
     private Button signin_button;
     private Button signup_button;
     private EditText emailAddress;
@@ -44,18 +45,33 @@ public class User_LogIn_Fragment extends AppCompatActivity implements View.OnCli
         emailAddress = (EditText) findViewById(R.id.email_edited);
         password = (EditText) findViewById(R.id.pw_edited);
 
-        signin_button.setOnClickListener(this);
-        signup_button.setOnClickListener(this);
+        signin_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(User_LogIn_Fragment.this, "YOU CLICKED SIGN IN", Toast.LENGTH_LONG).show();
+                userSignin();
+            }
+        });
+
+        signup_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(User_LogIn_Fragment.this, "YOU CLICKED SIGN US",Toast.LENGTH_LONG).show();
+                userSignup();
+            }
+        });
     }
 
-    @Override
+   /* @Override
     public void onClick(View v) {
         if(v == signup_button){
+            Toast.makeText(this, "YOU CLICKED SIGN UP", Toast.LENGTH_LONG).show();
             userSignup();
         } else if (v == signin_button){
+            Toast.makeText(this, "YOU CLICKED SIGN IN", Toast.LENGTH_LONG).show();
             userSignin();
         }
-    }
+    }*/
 
     private void userSignup(){
         String email = emailAddress.getText().toString().trim();
