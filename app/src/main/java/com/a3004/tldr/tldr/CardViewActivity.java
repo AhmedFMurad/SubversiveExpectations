75ad322ed9b4c8063ed97ecd25cf96187e8c12fe
ahmedfmmurad <ahmadfayez93@gmail.com>
2017-11-01 13:31:57 -0400

    list view attempt #1

--- app/src/main/java/com/a3004/tldr/tldr/CardViewActivity.java
+++ app/src/main/java/com/a3004/tldr/tldr/CardViewActivity.java
@@ -2,12 +2,49 @@ package com.a3004.tldr.tldr;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class CardViewActivity extends Activity {
+import java.util.List;
 
-    ImageView articleImage;
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.L;
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.T;
+
+public class CardViewActivity extends ArrayAdapter<String> {
+    private Activity context;
+    private List<Article> articles;
+
+    public CardViewActivity(Activity context, List<Article> articles){
+        super(context, R.layout.item);
+        this.context = context;
+        this.articles = articles;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        LayoutInflater inflater = context.getLayoutInflater();
+
+        View cardListView = inflater.inflate(R.layout.item, null, true);
+        TextView textViewTitle = (TextView) cardListView.findViewById(R.id.title1);
+        TextView textViewContent = (TextView) cardListView.findViewById(R.id.content1);
+
+        Article article = articles.get(position);
+
+        textViewTitle.setText(article.getArticleTitle());
+        textViewContent.setText(article.getArticleDescription());
+
+        return cardListView;
+    }
+
+    /*ImageView articleImage;
     TextView articleCategory;
     TextView articleText;
 
@@ -23,5 +60,5 @@ public class CardViewActivity extends Activity {
         articleImage.setImageResource(R.drawable.pepe1);
         articleCategory.setText("-- Default Category --");
         articleText.setText("-- Default Text --");
-    }
+    }*/
 }
