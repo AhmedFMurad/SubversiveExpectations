22714915605c30bf09457f0d9280c2777d99abe7
Jeffrey <jeffreylim@outlook.com>
2017-11-03 12:14:55 -0400

    Prize class

---
+++ app/src/main/java/com/a3004/tldr/tldr/Prizes.java
@@ -0,0 +1,18 @@
+package com.a3004.tldr.tldr;
+
+/**
+ * Created by Jeffrey on 11-03-2017.
+ */
+
+public class Prizes
+    private String name;
+    private int prize;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrize() {
+        return prize;
+    }
+}
