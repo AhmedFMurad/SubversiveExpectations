0f808cc81db0ab1d47dc620e2c7b260c9d3e8b05
Jeffrey <jeffreylim@outlook.com>
2017-11-27 17:31:19 -0500

    added viewsummaryadapter

--- app/src/main/java/com/a3004/tldr/tldr/ViewSummaryAdapter.java
+++ app/src/main/java/com/a3004/tldr/tldr/ViewSummaryAdapter.java
@@ -1,8 +1,16 @@
 package com.a3004.tldr.tldr;
 
+import android.content.Context;
+
+import java.util.ArrayList;
+
 /**
  * Created by Jeffrey on 11-27-2017.
  */
 
 public class ViewSummaryAdapter {
+    Context mContext;
+    private ArrayList<Summary> summaries;
+
+
 }
