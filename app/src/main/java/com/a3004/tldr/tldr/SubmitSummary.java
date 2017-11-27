bfdc416feb326d84d6ee3a02cedbe779e6267694
ahmedfmmurad <ahmadfayez93@gmail.com>
2017-11-27 17:59:13 -0500

    summary stuff

--- app/src/main/java/com/a3004/tldr/tldr/SubmitSummary.java
+++ app/src/main/java/com/a3004/tldr/tldr/SubmitSummary.java
@@ -4,10 +4,15 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -16,20 +21,25 @@ import com.google.firebase.database.ValueEventListener;
 import com.prof.rssparser.Article;
 import com.squareup.picasso.Picasso;
 
+import java.util.ArrayList;
+
 public class SubmitSummary extends AppCompatActivity {
     private FirebaseDatabase mFirebaseDatabase;
     private DatabaseReference mDatabaseReference;
+    private FirebaseAuth mFirebaseAuth;
     private String url;
     Context context = this;
     TextView content;
     ImageView image;
     TextView title;
-
-
+    TextInputEditText submission;
+    Button submit;
     public void initFirebase(){
         FirebaseApp.initializeApp(this);
         mFirebaseDatabase = FirebaseDatabase.getInstance();
-        mDatabaseReference = mFirebaseDatabase.getReference().child("categories").child("technology");
+        mDatabaseReference = mFirebaseDatabase.getReference();
+        //.child("categories").child("technology");
+        mFirebaseAuth = FirebaseAuth.getInstance();
 
     }
 
@@ -43,12 +53,34 @@ public class SubmitSummary extends AppCompatActivity {
         image = (ImageView) findViewById(R.id.image_view);
         title = (TextView) findViewById(R.id.title);
         content = (TextView) findViewById(R.id.content);
+        submission = (TextInputEditText) findViewById(R.id.summary_text2);
+        submit = (Button) findViewById(R.id.submit_button);
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Summary summary = new Summary();
+                ArrayList<String> empty = new ArrayList<String>();
+                summary.setDownvotes(empty);
+                summary.setUpvotes(empty);
+                summary.setUID(mFirebaseAuth.getCurrentUser().getUid());
+                summary.setContent(submission.getText().toString().trim());
+                mDatabaseReference = mFirebaseDatabase.getReference();
+                mDatabaseReference.child("summaries").child(url)
+                        .child(mFirebaseAuth.getCurrentUser().getUid()).child("summary")
+                        .setValue(summary);
+                Intent intent = new Intent(context, ViewSummaries.class);
+                intent.putExtra("newUrl", url);
+                context.startActivity(intent);
+            }
+        });
+
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        mDatabaseReference = mDatabaseReference.child(url);
+        mDatabaseReference = mDatabaseReference.child("categories").child("technology").child(url);
         mDatabaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
--- app/src/main/java/com/a3004/tldr/tldr/Summary.java
+++ app/src/main/java/com/a3004/tldr/tldr/Summary.java
@@ -1,14 +1,49 @@
 package com.a3004.tldr.tldr;
 
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Summary {
-    protected Map<String, Integer> vote;
+    private ArrayList<String> upvotes;
+    private ArrayList<String> downvotes;
     private String UID;
+    private String content;
 
     public Summary() {
 
+    }
+
+    public ArrayList<String> getUpvotes() {
+        return upvotes;
+    }
+
+    public ArrayList<String> getDownvotes() {
+        return downvotes;
+    }
+
+    public String getUID() {
+        return UID;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setUpvotes(ArrayList<String> upvotes) {
+        this.upvotes = upvotes;
+    }
+
+    public void setDownvotes(ArrayList<String> downvotes) {
+        this.downvotes = downvotes;
+    }
+
+    public void setUID(String UID) {
+        this.UID = UID;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
     }
 }
